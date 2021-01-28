6444a54e0cfcbc98ae9897e23f57b70db175ddd5
VijayTheGr8 <vijayspatnaik@gmail.com>
2021-01-27 20:27:10 -0500

    Fixed bug in navbar animation

--- UI-Angular/src/app/navbar/navbar.component.ts
+++ UI-Angular/src/app/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  // listens for a click
+  @HostListener('click', ['$event']) onClick() {
+      document
+      .querySelector('#mobile-menu')
+        .classList.toggle('is-active');
+      document
+      .querySelector('.navbar__menu')
+        .classList.toggle('active')
   }
 
+  ngOnInit(): void {}
 }
